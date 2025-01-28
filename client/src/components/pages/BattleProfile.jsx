02c8a597412308c88a5f40c9a09df25cbff47b93
Jocelyn Zhao <zhaojocelyn300@gmail.com>
2025-01-28 16:12:37 -0500

    latest fix

--- client/src/components/pages/BattleProfile.jsx
+++ client/src/components/pages/BattleProfile.jsx
@@ -258,7 +258,7 @@ const BattleProfile = (props) => {
               {displayedResults?.map((entry, index) => (
                 <tr key={index} className={index === 0 ? "BattleEnd-recent-battle" : ""}>
                   <td>
-                    {index === 0 && <span className="BattleEnd-recent-label">Latest</span>}
+                    {index === 0 && userInfo?.log?.length > 0 && <span className="BattleEnd-recent-label">Latest</span>}
                     <span className={`result-text ${entry.Result === "Win" ? "win" : "lose"}`}>
                       {entry.Result}
                     </span>
