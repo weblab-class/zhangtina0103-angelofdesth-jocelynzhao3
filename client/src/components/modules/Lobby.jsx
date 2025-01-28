e5fd7c60aa478cfbb8246c2ad9192fec72b6a6c9
Jocelyn Zhao <zhaojocelyn300@gmail.com>
2025-01-28 01:10:28 -0500

    v7

--- client/src/components/modules/Lobby.jsx
+++ client/src/components/modules/Lobby.jsx
@@ -29,21 +29,27 @@ const Lobby = (props) => {
 
   useEffect(() => {
     const newlobby = props.activeLobbies.find((lobbyc) => lobbyc.lobbyid === props.lobbyid);
+    if (!newlobby && props.lobbyid !== "newPVP" && props.lobbyid !== "newBot") {
+      // Lobby no longer exists, clear the display
+      props.setDisplayedLobby("");
+      props.setInLobby(false);
+      return;
+    }
+
     if (newlobby) {
       props.formatPlayerDisplay(setP1, newlobby.p1);
       props.formatPlayerDisplay(setP2, newlobby.p2);
-    }
-  }, [props.activeLobbies, props.lobbyid]);
 
-  useEffect(() => {
-    const newlobby = props.activeLobbies.find((lobbyc) => lobbyc.lobbyid === props.lobbyid);
-    if (newlobby && newlobby.p1ready && newlobby.p2ready) {
-      navigate("/battle/" + newlobby.lobbyid);
+      if (newlobby.p1ready && newlobby.p2ready) {
+        navigate("/battle/" + newlobby.lobbyid);
+      }
     }
   }, [props.activeLobbies, props.lobbyid]);
 
   const handleJoinClick = () => {
     const newlobby = props.activeLobbies.find((lobbyc) => lobbyc.lobbyid === props.lobbyid);
+    if (!newlobby) return; // Don't try to join if lobby doesn't exist
+
     post("/api/joinlobby", { lobbyid: newlobby.lobbyid, player: userInfo._id }).then((result) => {
       console.log("joined result: ", result);
     });
@@ -51,6 +57,8 @@ const Lobby = (props) => {
 
   const handleReadyClick = () => {
     const newlobby = props.activeLobbies.find((lobbyc) => lobbyc.lobbyid === props.lobbyid);
+    if (!newlobby) return; // Don't try to ready up if lobby doesn't exist
+
     post("/api/updateReadyStatus", {
       lobbyid: newlobby.lobbyid,
       player: userInfo._id,
@@ -62,6 +70,13 @@ const Lobby = (props) => {
 
   const handleLeaveClick = () => {
     const newlobby = props.activeLobbies.find((lobbyc) => lobbyc.lobbyid === props.lobbyid);
+    if (!newlobby) {
+      // If lobby doesn't exist anymore, just clear the frontend state
+      props.setInLobby(false);
+      props.setDisplayedLobby("");
+      return;
+    }
+
     post("/api/leaveLobby", { lobbyid: newlobby.lobbyid, player: userInfo._id }).then((result) => {
       console.log("leave result: ", result);
       props.setInLobby(false);
@@ -69,8 +84,15 @@ const Lobby = (props) => {
     });
   };
 
-  // Check if the current user has joined this lobby
+  // Get current lobby data
   const newlobby = props.activeLobbies.find((lobbyc) => lobbyc.lobbyid === props.lobbyid);
+  
+  // If lobby doesn't exist and it's not a creation screen, don't render anything
+  if (!newlobby && props.lobbyid !== "newPVP" && props.lobbyid !== "newBot") {
+    return null;
+  }
+
+  // Check if the current user has joined this lobby
   const hasJoined = newlobby && (userInfo._id === newlobby.p1 || userInfo._id === newlobby.p2);
   
   // Check if the current user is ready
--- client/src/components/pages/Start.jsx
+++ client/src/components/pages/Start.jsx
@@ -101,7 +101,7 @@ const Start = (props) => {
         </div>
       )}
       <div className="Start-content">
-        <h1 className="Start-title"> BattleLingo v6</h1>
+        <h1 className="Start-title"> BattleLingo v7</h1>
         {!userContext.userId && <div className="signin-prompt">Sign in to start battling</div>}
 
         <div className="google-login-container">
